dac95ce4cad2d70f84bd560f92f808db255f8e25
Anusha Mahajan <anushamahajan505@gmail.com>
2024-07-12 12:03:31 -0700

    Initial Commit

---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by Anusha Mahajan on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
